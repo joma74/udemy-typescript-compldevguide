8f75a0612c68f9934c61d0712d9f37055ab7b87d
joma74 <dev-mails@gmx.net>
2020-09-09 23:22:49 +0200

    docs: Tell why NonShadowLitElement

--- src/views/NonShadowLitElement.ts
+++ src/views/NonShadowLitElement.ts
@@ -1,9 +1,16 @@
 import { LitElement } from "lit-element"
 
 export abstract class NonShadowLitElement extends LitElement {
+	/**
+	 * Creates a non-shadow root.
+	 * See https://css-tricks.com/web-standards-meet-user-land-using-css-in-js-to-style-custom-elements/#applying-styles-for-litelement
+	 */
 	createRenderRoot() {
 		return this
 	}
+	/**
+	 * Well, it is a recommendation
+	 */
 	connectedCallback() {
 		super.connectedCallback()
 	}
