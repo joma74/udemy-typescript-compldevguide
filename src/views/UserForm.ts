b5146d0c1d489642b010d89a17d1a6ebe66aca47
joma74 <dev-mails@gmx.net>
2020-10-27 09:18:34 +0100

    feat: Use decorators for properties Constructor did not work

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,7 +1,13 @@
-import { html } from "lit-element"
+import { html, property } from "lit-element"
 import { NonShadowLitElement } from "./NonShadowLitElement"
 
 export class UserForm extends NonShadowLitElement {
+	@property()
+	name = "???"
+
+	@property({ type: Number })
+	age = -1
+
 	template = html`<form onsubmit="return false;">
 		<div class="m-2 p-5 bg-gray-100 rounded shadow-xl text-sm">
 			<h1 class="text-gray-800 font-medium underline bg-yellow-300">
@@ -10,8 +16,7 @@ export class UserForm extends NonShadowLitElement {
 			<!-- display: inline --><label
 				class="inline-block mt-2 pl-1 text-gray-700 w-1/4"
 				for="name"
-			>
-				Name </label
+				>Name</label
 			><!-- display: inline --><div class="inline-block mt-2 w-3/4"
 				><!-- display: inline --><input
 					class="inline-block pl-1 px-5 text-gray-700 bg-gray-200 rounded italic shadow-md w-1/2"
@@ -19,33 +24,34 @@ export class UserForm extends NonShadowLitElement {
 					id="name"
 					name="name"
 					type="text"
-					placeholder="Sam"
+					placeholder="${this.name}"
 					aria-label="Name"
 				/><!-- display: inline --><button
 					class="hover:bg-yellow-300 inline-block pl-1 px-5 tracking-wider rounded bg-gray-100 text-xs text-center italic border-gray-700 border-2 shadow-md w-1/2"
-					style="transform: skew(-2deg)"
+					style="transform: skew(-4deg)"
 					type="submit"
+					@click="${this.onUpdateNameClicked}"
+					>Update Name</button
 				>
-					Update Name
-				</button>
 			</div>
 			<label class="inline-block mt-2 pl-1 text-gray-700 w-1/4"> Age </label
 			><!-- display: inline --><div class="inline-block mt-2 w-3/4">
 				<!-- display: inline --><span
 					class="inline-block pl-1 px-5 rounded w-1/2"
-				></span
+					>${this.age}</span
 				><!-- display: inline --><button
 					class="hover:bg-yellow-300 inline-block pl-1 px-5 tracking-wider rounded bg-gray-100 text-xs text-center italic border-gray-700 border-2 shadow-md w-1/2"
-					style="transform: skew(-2deg)"
+					style="transform: skew(-4deg)"
 					type="submit"
+					@click="${this.onSetRandomClicked}"
+					>Set Random</button
 				>
-					Set Random
-				</button>
 			</div>
 			<!-- display: inline --><button
 				class="hover:bg-yellow-300 inline-block mt-4 px-5 py-1 tracking-wider rounded bg-white text-sm text-center italic font-semibold border-black border-2 shadow-md w-1/4"
 				style="transform: skew(-4deg)"
 				type="submit"
+				@click="${this.onSaveClicked}"
 				>Save</button
 			><!-- display: inline --><div class="inline-block mt-4 w-3/4"></div>
 		</div>
@@ -53,6 +59,24 @@ export class UserForm extends NonShadowLitElement {
 
 	render() {
 		return this.template
+	}
+
+	private onUpdateNameClicked(e: Event) {
+		console.log(e.target)
+	}
+
+	private onSetRandomClicked(e: Event) {
+		console.log(e.target)
+	}
+
+	private onSaveClicked(e: Event) {
+		console.log(e.target)
+	}
+
+	updated(changedProperties: any) {
+		changedProperties.forEach((oldValue: any, propName: string) => {
+			console.log(`${propName} changed. oldValue: ${oldValue}`)
+		})
 	}
 }
 
--- src/views/UserInfo.ts
+++ src/views/UserInfo.ts
@@ -9,13 +9,11 @@ export class UserInfo extends NonShadowLitElement {
 		<h1 id="root"></h1>
 		<!-- display: inline --><div
 			class="inline-block mt-2 pl-1 w-1/4 text-gray-700"
-		>
-			Name </div
+			>Name</div
 		><!-- display: inline --><div class="inline-block mt-2 w-3/4">Sam</div
 		><!-- display: inline --><div
 			class="inline-block mt-2 pl-1 text-gray-700 w-1/4"
-		>
-			Age </div
+			>Age</div
 		><!-- display: inline --><div class="inline-block mt-2 w-3/4">20</div>
 	</div>`
 
